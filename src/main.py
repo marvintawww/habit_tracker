d3c3696a676beb9dd80a754f2c0ccee480e4776a
marvintawww <marvintawww@gmail.com>
2026-03-17 14:14:19 +0300

    third commit from vim)))

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from src.routes.v1.__init__ import router as main_router
+from src.database.db import db
+from src.models.user import User
+from src.models.jwt import JWTBlacklist
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db.create_tables()
+    yield
+    await db.engine.dispose()
+
 
 app = FastAPI(
     title="Habit Tracker",
